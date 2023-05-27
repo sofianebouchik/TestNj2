0e4e02c52bbc85aa99e8f0e708226bdef6acc036
sofianebouchik <sosobouchik@gmail.com>
2023-05-27 18:02:17 -0400

    secondCommit

--- src/test/java/ClassDataProvider.java
+++ src/test/java/ClassDataProvider.java
@@ -3,9 +3,11 @@ import org.testng.annotations.DataProvider;
 public class ClassDataProvider {
     @DataProvider(name = "data")
     public static Object[][] dtProvider(){
-        Object[][] a=new Object[1][2];
+        Object[][] a=new Object[2][7];
         a[0][0]="5";
         a[0][1]="6";
+        a[1][0]="13";
+        a[1][1]="9";
         return a;
 
     }
